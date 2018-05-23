35c475372fd7e32cf0d5348a584ff2552d523087
Freddie <xoptimal@163.com>
2018-05-23 16:05:27 +0800

    Initial commit

---
+++ coverlayout/src/main/java/com/xoptimal/widget/CoverLayout.kt
@@ -0,0 +1,14 @@
+package com.xoptimal.widget
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+/**
+ * Created by Freddie on 2018/5/23 0023 .
+ * Description:
+ */
+class CoverLayout : FrameLayout {
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+}
